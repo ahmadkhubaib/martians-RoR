162db29d821d59f218c225ccaf82985d13bfdca9
khubaib <khubaibahmad1994@gmail.com>
2021-03-14 12:53:22 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+john = User.create!(
+  first_name: 'John',
+  last_name: 'connor',
+  email: 'john@martian.com'
+)
+
+sarah = User.create!(
+  first_name: 'Sarah',
+  last_name: 'connor',
+  email: 'sarah@martian.com'
+)
+
+Item.create!(
+  [
+    {
+      title: 'Martian Chronicles',
+      description: 'Cult book by Ray Bradbury',
+      image_url: 'https://upload.wikimedia.org/wikipedia/en/4/45/The-Martian-Chronicles.jpg',
+      user: john
+    },
+    {
+      title: 'The Martian',
+      description: 'Novel by Andy Weir about an astronaut stranded on Mars trying to survive',
+      image_url: 'https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg',
+      user: sarah
+    },
+    {
+      title: 'Doom',
+      description: 'A group of Marines is sent to the red planet via an ancient Martian portal called the Ark to deal with an outbreak of a mutagenic virus',
+      image_url: 'https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg',
+      user: john
+    },
+    {
+      title: 'Mars Attacks!',
+      description: 'Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor',
+      image_url: 'https://upload.wikimedia.org/wikipedia/en/b/bd/Mars_attacks_ver1.jpg',
+      user: sarah
+    }
+  ]
+)
